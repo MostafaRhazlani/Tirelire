c6f87fcad61980972f10f3cc39ca0ffaad89a88b
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-10-13 16:33:47 +0100

    TIR-18 -> create schema of group

---
+++ src/models/group.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const groupSchema = new mongoose.Schema({
+    groupName: String,
+    description: String,
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    members: [{
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+        role: { type: String, enum: ["owner", "member"], default: "member" },
+        joinTimeScore: Number // score at time of joining
+    }],
+    rules: {
+        contributionAmount: Number,
+        contributionFrequency: { type: String, enum: ["weekly", "monthly"], default: "weekly" },
+        deadlineDays: Number
+    },
+}, { timestamps: true });
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
+
